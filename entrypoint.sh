edf624a26d8a7447f5de74bb5dc5911e0ed9efef
Ladicle <Ladiclexxx@gmail.com>
2016-07-18 17:48:50 +0900

    Fix method to check elasticsearch status

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # Wait for the Elasticsearch container to be ready before starting Kibana.
 echo "Stalling for Elasticsearch"
 while true; do
-    nc -q 1 elasticsearch 9200 2>/dev/null && break
+    printf "HEAD / HTTP/1.0\n\n" | nc elasticsearch 9200 >/dev/null 2>&1 && break
 done
 
 echo "Register index template to Elasticsearch"
